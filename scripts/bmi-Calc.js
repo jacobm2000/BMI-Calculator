cdd50b65ccb39386cb094e2b04d708c26f7ed769
jacobm2000 <maurerja2000@gmail.com>
2023-07-22 15:11:12 -0400

    Added Functionality of BMI calculation
    Now when calculate is pressed the users BMI is calculated and they are told if  they are underweight, normal, obese, or severely obese.

--- scripts/bmi-Calc.js
+++ scripts/bmi-Calc.js
@@ -1,2 +1,41 @@
 var btn= document.getElementById("calc-btn")
 var bmiOut=document.getElementById("bmi")
+var height=document.getElementById("height")
+var ft=document.getElementById("ft")
+var inch=document.getElementById("in")
+var bmiClass=document.getElementById("bmi-class")
+
+function calc(){
+    //check to see if inputs are numeric and not empty
+    if(isNaN(weight.value)||weight.value===""|isNaN(ft.value)|| ft.value===""|| isNaN(inch.value)||inch.value==undefined){
+        alert("at least one of your values is not a number please enter only numbers")
+    }
+    else{
+        let tinch=(parseInt(ft.value)*12)+parseFloat(inch.value)
+        let bmi=((parseFloat(weight.value)/tinch)/71)*703
+        bmiOut.textContent="your bmi is: "+bmi.toFixed(2)
+        if(bmi<18.5){
+            bmiClass.textContent=" Your Bmi is: Underweight"
+        }
+        else if(bmi>18.5 && bmi<=24.99){
+            bmiClass.textContent=" Your Bmi is: Normal"
+        }
+        else if(bmi>24.99 && bmi<=29.99){
+            bmiClass.textContent=" Your Bmi is: Overweight"
+        }
+        else if(bmi>29.99 && bmi<34.99){
+            bmiClass.textContent=" Your Bmi is: Obese"
+        }
+        else if( bmi>34.99){
+            bmiClass.textContent=" Your Bmi is: Severely Obese"
+        }
+        
+    }
+  
+
+
+}
+
+
+
+btn.addEventListener("click",calc)
