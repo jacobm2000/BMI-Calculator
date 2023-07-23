48c39db6ad1dd098e68876f17b1307177a470121
jacobm2000 <maurerja2000@gmail.com>
2023-07-23 10:11:37 -0400

    added unit changing
    designed a new interfaces for both imperial and metric units and then coded it so the user can switch between the two

--- scripts/bmi-Calc.js
+++ scripts/bmi-Calc.js
@@ -1,41 +1,85 @@
-var btn= document.getElementById("calc-btn")
-var bmiOut=document.getElementById("bmi")
-var height=document.getElementById("height")
-var ft=document.getElementById("ft")
-var inch=document.getElementById("in")
-var bmiClass=document.getElementById("bmi-class")
+var btn = document.getElementById("calc-btn")
+var bmiOut = document.getElementById("bmi")
+var height = document.getElementById("height")
+var ft = document.getElementById("ft")
+var inch = document.getElementById("in")
+var heightCM = document.getElementById("heightCM")
+var bmiClass = document.getElementById("bmi-class")
+var unitSelect = document.getElementById("units")
+var weightLabel = document.getElementById("weightLabel")
+var heightMetric = document.getElementById("hm")
+var heightImperial = document.getElementById("hi")
 
-function calc(){
+function units() {
+    if (unitSelect.value === "Imperial") {
+        weight.value = ""
+        heightCM.value = ""
+        heightMetric.style.display = "none"
+        weightLabel.textContent = "Weight (lb)"
+        heightImperial.style.display = "inherit"
+
+    }
+    if (unitSelect.value === "Metric") {
+        weight.value = ""
+        ft.value = ""
+        inch.value = ""
+        heightMetric.style.display = "inherit"
+        weightLabel.textContent = "Weight (Kg)"
+        heightImperial.style.display = " none"
+
+
+    }
+
+}
+
+
+function calc() {
     //check to see if inputs are numeric and not empty
-    if(isNaN(weight.value)||weight.value===""|isNaN(ft.value)|| ft.value===""|| isNaN(inch.value)||inch.value==undefined){
-        alert("at least one of your values is not a number please enter only numbers")
+    if (unitSelect.value === "Imperial") {
+        if (isNaN(weight.value) || weight.value === "" || isNaN(ft.value) || ft.value === "" || isNaN(inch.value) || inch.value == "") {
+            alert("at least one of your values is not a number please enter only numbers")
+            return
+        }
+        else {
+            let tinch = (parseInt(ft.value) * 12) + parseFloat(inch.value)
+            var bmi = ((parseFloat(weight.value) / tinch) / 71) * 703
+        }
     }
-    else{
-        let tinch=(parseInt(ft.value)*12)+parseFloat(inch.value)
-        let bmi=((parseFloat(weight.value)/tinch)/71)*703
-        bmiOut.textContent="Your BMI is: "+bmi.toFixed(2)
-        if(bmi<18.5){
-            bmiClass.textContent="BMI Catagory: Underweight"
+    else {
+        if (isNaN(weight.value) || weight.value === "" || isNaN(heightCM.value) || heightCM.value === "") {
+            alert("at least one of your values is not a number please enter only numbers")
+            return
         }
-        else if(bmi>18.5 && bmi<=24.99){
-            bmiClass.textContent="BMI Catagory: Normal"
+        else {
+            let cm = (parseFloat(heightCM.value))
+            var bmi = ((parseFloat(weight.value) / cm) / cm) * 10000
         }
-        else if(bmi>24.99 && bmi<=29.99){
-            bmiClass.textContent="BMI Catagory: Overweight"
-        }
-        else if(bmi>29.99 && bmi<34.99){
-            bmiClass.textContent="BMI Catagory: Obese"
-        }
-        else if( bmi>34.99){
-            bmiClass.textContent="BMI Catagory: Severely Obese"
-        }
-        
     }
-  
+    bmiOut.textContent = "Your BMI is: " + bmi.toFixed(2)
+
+    if (bmi < 18.5) {
+        bmiClass.textContent = "BMI Catagory: Underweight"
+    }
+    else if (bmi > 18.5 && bmi <= 24.99) {
+        bmiClass.textContent = "BMI Catagory: Normal"
+    }
+    else if (bmi > 24.99 && bmi <= 29.99) {
+        bmiClass.textContent = "BMI Catagory: Overweight"
+    }
+    else if (bmi > 29.99 && bmi < 34.99) {
+        bmiClass.textContent = "BMI Catagory: Obese"
+    }
+    else if (bmi > 34.99) {
+        bmiClass.textContent = "BMI Catagory: Severely Obese"
+    }
+
 
 
 }
 
 
 
-btn.addEventListener("click",calc)
+
+
+unitSelect.addEventListener("change", units)
+btn.addEventListener("click", calc)
